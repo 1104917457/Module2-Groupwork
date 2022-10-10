0617b8bb38a05e2d03546f529f0d6e91225475c8
zhoushudarkages <115099173+zhoushudarkages@users.noreply.github.com>
2022-10-10 16:50:58 -0500

    Use better model

--- R_code/STAT628_BODYFAT_DEMO.R
+++ R_code/STAT628_BODYFAT_DEMO.R
@@ -1,101 +1,124 @@
 library(ggplot2)
 
 ###Data loading
-BodyFat = read.csv("../data/BodyFat.csv") 
+BodyFat = read.csv("BodyFat.csv") 
 
 
 ###Data preprocess
 #BodyFat calculated by density
+#BMI calculated by weight and height
+inequalList1 = BodyFat$IDNO[abs(BodyFat$BODYFAT-BodyFat$B)>2]
+inequalList2 = BodyFat$IDNO[abs(BodyFat$BMI-BodyFat$ADIPOSITY)>1]
 BodyFat$BODYFAT_cal = 495/BodyFat$DENSITY-450  
+BodyFat$BMI = BodyFat$WEIGHT*0.4536/(BodyFat$HEIGHT*0.0254)^2
 
-#Find and remove the error data of BodyFat
-inequalList1 = BodyFat$IDNO[abs(BodyFat$BODYFAT-BodyFat$BODYFAT_cal)>2]
-BodyFat = BodyFat[-which(abs(BodyFat$BODYFAT-BodyFat$BODYFAT_cal)>2),]
+#remove the error data of BodyFat and BMI
+BodyFat=BodyFat[-which(abs(BodyFat$BODYFAT-BodyFat$BODYFAT_cal)>2),]
+BodyFat=BodyFat[-which(abs(BodyFat$BMI-BodyFat$ADIPOSITY)>1),]
 
 
 ###Model building
-#choose initial model
-#Relationship between Bodyfat and Adiposity
-ggplot(BodyFat,aes(ADIPOSITY,BODYFAT))+
-        geom_point()+
-        geom_smooth(method="lm",se=FALSE)+
-        labs(
-                x="Adiposity",
-                y="Body Fat %",
-                title="Scatterplot of Body Fat % and Adiposity"
-        )
-
-#Relationship between Bodyfat and Abdomen
+#Model1:Bodyfat ~ ADIPOSITY
+#Relationship between Bodyfat and ABDOMEN
 ggplot(BodyFat,aes(ABDOMEN,BODYFAT))+
         geom_point()+
-        geom_smooth(method="lm",se=FALSE)+
+        stat_smooth(method="lm",se=FALSE)+
         labs(
-                x="Abdomen",
+                x="BMI",
                 y="Body Fat %",
-                title="Scatterplot of Body Fat % and Abdomen"
+                title="Scatterplot of Body Fat % and ABDOMEN"
         )
 
-#Relationship between Adiposity and Abdomen, finding collinearity
-ggplot(BodyFat,aes(ABDOMEN,ADIPOSITY))+
-        geom_point()+
-        geom_smooth(method="lm",se=FALSE)+
-        labs(
-                x="Abdomen",
-                y="Adiposity",
-                title="Scatterplot of Adiposity and Abdomen"
-        )
+lmmodel = lm(BODYFAT ~ ABDOMEN, data=BodyFat)
+summary(lmmodel)
 
-#Compare R square of the two model
-lmmodel1_1 = lm(BODYFAT ~ ADIPOSITY, data=BodyFat)
-summary(lmmodel1_1)
-lmmodel1_2 = lm(BODYFAT ~ ABDOMEN, data=BodyFat)
-summary(lmmodel1_2)
-
-#The initial model is Bodyfat ~ Abdomen
-lmmodel1=lmmodel1_2
-
-#Model2:Bodyfat ~ Abdomen without influential point   (improved model1)
+#Model2:Bodyfat ~ ABDOMEN without outlier   (improved model1)
 BodyFat2=BodyFat[-which(abs(BodyFat$ABDOMEN)>120),]
+outlierList = BodyFat$IDNO[abs(BodyFat$ABDOMEN)>120]
 ggplot(BodyFat2,aes(ABDOMEN,BODYFAT))+
         geom_point()+
         stat_smooth(method="lm",se=FALSE)+
         labs(
-                x="Abdomen",
+                x="BMI",
                 y="Body Fat %",
-                title="Scatterplot of Body Fat % and Abdomen without influential point"
+                title="Scatterplot of Body Fat % and BMI without outlier"
         )
 
 lmmodel2 = lm(BODYFAT ~ ABDOMEN, data=BodyFat2)
 summary(lmmodel2)
 
 
-
-
 #Model3:add another covariate to model2
 #using added variable plot
-par(mfrow = c(2, 2))
+#waiting for more debate
 fit=lm(AGE~ABDOMEN,data=BodyFat2)
 plot(fit$residuals,lmmodel2$residuals,pch=19)
 lmage = lm(lmmodel2$residuals ~ fit$residuals)
 summary(lmage)
 abline(lmage,col="blue",lwd=5)
 
-fit=lm(NECK~ABDOMEN,data=BodyFat2)
-plot(fit$residuals,lmmodel2$residuals,pch=19)
-lmage = lm(lmmodel2$residuals ~ fit$residuals)
-summary(lmage)
-abline(lmage,col="blue",lwd=5)
+fit2=lm(ADIPOSITY~ABDOMEN,data=BodyFat2)
+plot(fit2$residuals,lmmodel2$residuals,pch=19)
+lmbmi = lm(lmmodel2$residuals ~ fit2$residuals)
+summary(lmbmi)
+abline(lmbmi,col="blue",lwd=5)
 
 
-#Model3_1:add Age variable
-lmmodel3_1 = lm(BODYFAT ~ ABDOMEN + AGE, data=BodyFat2)
-summary(lmmodel3_1)
+lmmodel3 = lm(BODYFAT ~ ABDOMEN + AGE, data=BodyFat2)
+summary(lmmodel3)
 
-#Model3_2:add Neck variable
-lmmodel3_2 = lm(BODYFAT ~ ABDOMEN + AGE, data=BodyFat2)
-summary(lmmodel3_2)
+#Model4:add another covariate to model3
+#using added variable plot
+#waiting for more debate
 
-###Model comparison
+fit3=lm(CHEST~ABDOMEN,data=BodyFat2)
+plot(fit3$residuals,lmmodel3$residuals,pch=19)
+lmchest = lm(lmmodel3$residuals ~ fit3$residuals)
+summary(lmchest)
+abline(lmchest,col="blue",lwd=5)
+
+fit4=lm(HIP~ABDOMEN,data=BodyFat2)
+plot(fit4$residuals,lmmodel3$residuals,pch=19)
+lmhip = lm(lmmodel3$residuals ~ fit4$residuals)
+summary(lmhip)
+abline(lmhip,col="blue",lwd=5)
+
+fit5=lm(THIGH~ABDOMEN,data=BodyFat2)
+plot(fit5$residuals,lmmodel3$residuals,pch=19)
+lmthigh = lm(lmmodel3$residuals ~ fit5$residuals)
+summary(lmthigh)
+abline(lmthigh,col="blue",lwd=5)
+
+fit6=lm(FOREARM~ABDOMEN,data=BodyFat2)
+plot(fit6$residuals,lmmodel3$residuals,pch=19)
+lmfore = lm(lmmodel3$residuals ~ fit6$residuals)
+summary(lmfore)
+abline(lmfore,col="blue",lwd=5)
+
+lmmodel4 = lm(BODYFAT ~ ABDOMEN + AGE + CHEST, data=BodyFat2)
+summary(lmmodel4)
+
+###Model diagnostics
+diagnostic = function(model,n){
+        par(mfrow = c(2, 2))
+        ##Standardized Residual Plot
+        plot(predict(model),resid(model),pch=19,main="Standardized Residual Plot")
+        abline(a=0,b=0,col="black",lwd=3)
+        ##Normal Q-Q Plot of the Residuals
+        qqnorm(rstandard(model),pch=19,
+               main="Normal Q-Q Plot of the Residuals")
+        abline(a=0,b=1,col="black",lwd=3)
+        ##Cook's distance
+        plot(model,4)
+        pii2 = hatvalues(lmmodel2)
+        plot(1:n,pii2,type="p",pch=19,main="Leverage Values (Pii)")
+}
+
+diagnostic(lmmodel2,242)
+diagnostic(lmmodel3,242)
+diagnostic(lmmodel4,242)
+
+
 #CrossValidation
 CV=function(n,Z){
         z=rep(1:Z,ceiling(n/Z))[1:n]
@@ -103,19 +126,30 @@ CV=function(n,Z){
         mm=list()
         for (i in 1:Z) mm[[i]]=(1:n)[z==i];return(mm)
 }
+
 nrow=nrow(BodyFat2)
 
-EEMS=function(t=3,Z=10){
+EEMS=function(t=3,Z=10,mod){
         EMSE = numeric(t)
         for(i in 1:t){
                 listcv=CV(nrow,Z)
                 MSE=numeric(Z)
-                for(j in 1:Z){   #ѭ??ʮ??
+                for(j in 1:Z){   
                         m=listcv[[j]]
                         train = BodyFat2[-m,]
                         pred = BodyFat2[m,]
-                        model3 = lm(BODYFAT ~ ADIPOSITY + AGE , data=train)
-                        predBodyFat = predict(model3, newdata = pred)
+                        if (mod == 'lmmodel2'){
+                                model = lm(BODYFAT ~ ABDOMEN , data=train)
+                                predBodyFat = predict(model, newdata = pred)
+                        }
+                        if (mod == 'lmmodel3'){
+                                model = lm(BODYFAT ~ ABDOMEN + AGE, data=train)
+                                predBodyFat = predict(model, newdata = pred)
+                        }
+                        if (mod == 'lmmodel4'){
+                                model = lm(BODYFAT ~ ABDOMEN + AGE + CHEST, data=train)
+                                predBodyFat = predict(model, newdata = pred)
+                        }
                         MSE[j]=sqrt(sum(predBodyFat - pred$BODYFAT)^2/length(predBodyFat))
                 }
                 EMSE[i] = mean(MSE)
@@ -124,25 +158,9 @@ EEMS=function(t=3,Z=10){
         return(EEE)
 }
 
-E=EEMS(100,10)
-E
-
-
-###Model diagnostics
-diagnostic = function(model){
-        par(mfrow = c(2, 2))
-        ##Standardized Residual Plot
-        plot(predict(model),resid(model),pch=19,main="Standardized Residual Plot",xlab = "Prediction",ylab = "Residual")
-        abline(a=0,b=0,col="black",lwd=3)
-        ##Normal Q-Q Plot of the Residuals
-        qqnorm(rstandard(model),pch=19,
-               main="Normal Q-Q Plot of the Residuals")
-        abline(a=0,b=1,col="black",lwd=3)
-        ##Cook's distance
-        plot(model,4)
-        ##Pii
-        pii4 = hatvalues(model)
-        plot(1:length(model$residual),pii4,type="p",pch=19,main="Leverage Values (Pii)",xlab = "Obs. number",ylab = "Hatvalues")
-}
-
-diagnostic(lmmodel2)
+E1=EEMS(100,5,'lmmodel2')
+E2=EEMS(100,5,'lmmodel3')
+E3=EEMS(100,5,'lmmodel4')
+E1
+E2
+E3
