ad0766ba5ec8576230f9739c0b80dbedd19af631
xzou <xzou42@wisc.edu>
2022-10-09 20:57:58 -0500

    modify,simplify the code and interpret

--- R_code/STAT628_BODYFAT_DEMO.R
+++ R_code/STAT628_BODYFAT_DEMO.R
@@ -1,87 +1,128 @@
-BodyFat = read.csv("BodyFat.csv") 
-dim(BodyFat)
-head(BodyFat)
-tail(BodyFat) 
-BodyFat$B = 495/BodyFat$DENSITY-450
-inequalList1 = BodyFat$IDNO[abs(BodyFat$BODYFAT-BodyFat$B)>2]
-BodyFat=BodyFat[-which(abs(BodyFat$BODYFAT-BodyFat$B)>2),]
-plot(BodyFat$BODYFAT,BodyFat$DENSITY,pch=19)
-BodyFat$BMI = BodyFat$WEIGHT*0.4536/(BodyFat$HEIGHT*0.0254)^2
-inequalList2 = BodyFat$IDNO[abs(BodyFat$BMI-BodyFat$ADIPOSITY)>1]
-BodyFat=BodyFat[-which(abs(BodyFat$BMI-BodyFat$ADIPOSITY)>1),]
-plot(BodyFat$BMI,BodyFat$ADIPOSITY,pch=19)
+library(ggplot2)
 
-plot(ADIPOSITY,BODYFAT,pch=19,cex=1.2,cex.lab=1.5,cex.main=1.5,
-     xlab="BMI",ylab="Body Fat %",main="Scatterplot of Body Fat % and Age")
+###Data loading
+BodyFat = read.csv("../data/BodyFat.csv") 
+
+
+###Data preprocess
+#BodyFat calculated by density
+#BMI calculated by wight and height
+BodyFat$BODYFAT_cal = 495/BodyFat$DENSITY-450  
+BodyFat$BMI = BodyFat$WEIGHT*0.4536/(BodyFat$HEIGHT*0.0254)^2
+
+#remove the error data of BodyFat and BMI
+BodyFat=BodyFat[-which(abs(BodyFat$BODYFAT-BodyFat$BODYFAT_cal)>2),]
+BodyFat=BodyFat[-which(abs(BodyFat$BMI-BodyFat$ADIPOSITY)>1),]
+
+
+###Model building
+#Model1:Bodyfat ~ BMI
+#Relationship between Bodyfat and BMI
+ggplot(BodyFat,aes(ADIPOSITY,BODYFAT))+
+        geom_point()+
+        stat_smooth(method="lm",se=FALSE)+
+        labs(
+                x="BMI",
+                y="Body Fat %",
+                title="Scatterplot of Body Fat % and BMI"
+        )
+
 lmmodel = lm(BODYFAT ~ ADIPOSITY, data=BodyFat)
-abline(lmmodel,col="blue",lwd=5) #lwd is the line weight and makes the line thicker.
-legend("bottomright",c("SLR Line"),col="blue",lwd=3,lty=c(1))
 summary(lmmodel)
 
+#Model2:Bodyfat ~ BMI without outlier   (improved model1)
 BodyFat2=BodyFat[-which(abs(BodyFat$BMI)>35),]
-plot(BodyFat2$ADIPOSITY,BodyFat2$BODYFAT,pch=19,cex=1.2,cex.lab=1.5,cex.main=1.5,
-     xlab="BMI",ylab="Body Fat %",main="Scatterplot of Body Fat % and Age")
+ggplot(BodyFat2,aes(ADIPOSITY,BODYFAT))+
+        geom_point()+
+        stat_smooth(method="lm",se=FALSE)+
+        labs(
+                x="BMI",
+                y="Body Fat %",
+                title="Scatterplot of Body Fat % and BMI without outlier"
+        )
+
 lmmodel2 = lm(BODYFAT ~ ADIPOSITY, data=BodyFat2)
-abline(lmmodel2,col="blue",lwd=5) #lwd is the line weight and makes the line thicker.
-legend("bottomright",c("SLR Line"),col="blue",lwd=3,lty=c(1))
 summary(lmmodel2)
 
-plot(predict(lmmodel2),resid(lmmodel2),pch=19,main="Standardized Residual Plot")
-abline(a=0,b=0,col="black",lwd=3)
 
-qqnorm(rstandard(lmmodel2),pch=19,
-       main="Normal Q-Q Plot of the Residuals")
-abline(a=0,b=1,col="black",lwd=3)
-
-pii2 = hatvalues(lmmodel2)
-cooki2 = cooks.distance(lmmodel2)
-
-n = dim(BodyFat2)[1]
-plot(1:n,pii2,type="p",pch=19,main="Leverage Values (Pii)")
-plot(1:n,cooki2,type="p",pch=19,main="Influence Values (Cook's Distance)")
-
-BodyFat2$model2res = BodyFat2$BODYFAT - predict(lmmodel2)
-plot(BodyFat2$AGE,BodyFat2$model2res,pch=19)
-lmage = lm(BodyFat2$model2res ~ BodyFat2$AGE)
+#Model3:add another covariate to model2
+#using added variable plot
+#waiting for more debate
+fit=lm(AGE~ADIPOSITY,data=BodyFat2)
+plot(fit$residuals,lmmodel2$residuals,pch=19)
+lmage = lm(lmmodel2$residuals ~ fit$residuals)
 summary(lmage)
 abline(lmage,col="blue",lwd=5)
+
+fit=lm(ABDOMEN~ADIPOSITY,data=BodyFat2)
+plot(fit$residuals,lmmodel2$residuals,pch=19)
+lmage = lm(lmmodel2$residuals ~ fit$residuals)
+summary(lmage)
+abline(lmage,col="blue",lwd=5)
+
+fit=lm(ANKLE~ADIPOSITY,data=BodyFat2)
+plot(fit$residuals,lmmodel2$residuals,pch=19)
+lmage = lm(lmmodel2$residuals ~ fit$residuals)
+summary(lmage)
+abline(lmage,col="blue",lwd=5)
+
+fit=lm(FOREARM~ADIPOSITY,data=BodyFat2)
+plot(fit$residuals,lmmodel2$residuals,pch=19)
+lmage = lm(lmmodel2$residuals ~ fit$residuals)
+summary(lmage)
+abline(lmage,col="blue",lwd=5)
+
 
 lmmodel3 = lm(BODYFAT ~ ADIPOSITY + AGE, data=BodyFat2)
 summary(lmmodel3)
 
-plot(predict(lmmodel3),resid(lmmodel3),pch=19,main="Standardized Residual Plot")
-abline(a=0,b=0,col="black",lwd=3)
 
-qqnorm(rstandard(lmmodel3),pch=19,
-       main="Normal Q-Q Plot of the Residuals")
-abline(a=0,b=1,col="black",lwd=3)
 
-pii3 = hatvalues(lmmodel3)
-cooki3 = cooks.distance(lmmodel3)
-plot(1:n,pii3,type="p",pch=19,main="Leverage Values (Pii)")
-plot(1:n,cooki3,type="p",pch=19,main="Influence Values (Cook's Distance)")
+###Model diagnostics
+diagnostic = function(model){
+        par(mfrow = c(2, 2))
+        ##Standardized Residual Plot
+        plot(predict(model),resid(model),pch=19,main="Standardized Residual Plot")
+        abline(a=0,b=0,col="black",lwd=3)
+        ##Normal Q-Q Plot of the Residuals
+        qqnorm(rstandard(model),pch=19,
+               main="Normal Q-Q Plot of the Residuals")
+        abline(a=0,b=1,col="black",lwd=3)
+        ##Cook's distance
+        plot(model,4)
+}
 
-BodyFat2$model3res = BodyFat2$BODYFAT - predict(lmmodel3)
-plot(BodyFat2$model2res,BodyFat2$model3res,pch=19)
+diagnostic(lmmodel2)
 
-plot(BodyFat2$WRIST,BodyFat2$model3res,pch=19)
-lmwr = lm(BodyFat2$model3res ~ BodyFat2$WRIST)
-summary(lmwr)
-abline(lmwr,col="blue",lwd=5)
+#CrossValidation
+CV=function(n,Z){
+        z=rep(1:Z,ceiling(n/Z))[1:n]
+        z=sample(z,n)
+        mm=list()
+        #mm[[i]]Ϊ??i???±꼯
+        for (i in 1:Z) mm[[i]]=(1:n)[z==i];return(mm)
+}
 
-lmmodel4 = lm(BODYFAT ~ ADIPOSITY + AGE + WRIST , data=BodyFat2)
-summary(lmmodel4)
+nrow=nrow(BodyFat2)
 
-plot(predict(lmmodel4),resid(lmmodel4),pch=19,main="Standardized Residual Plot")
-abline(a=0,b=0,col="black",lwd=3)
+EEMS=function(t=3,Z=10){
+        EMSE = numeric(t)
+        for(i in 1:t){
+                listcv=CV(nrow,Z)
+                MSE=numeric(Z)
+                for(j in 1:Z){   #ѭ??ʮ??
+                        m=listcv[[j]]
+                        train = BodyFat2[-m,]
+                        pred = BodyFat2[m,]
+                        model3 = lm(BODYFAT ~ ADIPOSITY + AGE , data=train)
+                        predBodyFat = predict(model3, newdata = pred)
+                        MSE[j]=sqrt(sum(predBodyFat - pred$BODYFAT)^2/length(predBodyFat))
+                }
+                EMSE[i] = mean(MSE)
+        }
+        EEE = mean(EMSE)
+        return(EEE)
+}
 
-qqnorm(rstandard(lmmodel4),pch=19,
-       main="Normal Q-Q Plot of the Residuals")
-abline(a=0,b=1,col="black",lwd=3)
-
-pii4 = hatvalues(lmmodel4)
-cooki4 = cooks.distance(lmmodel4)
-plot(1:n,pii4,type="p",pch=19,main="Leverage Values (Pii)")
-plot(1:n,cooki4,type="p",pch=19,main="Influence Values (Cook's Distance)")
-
-BodyFat2$model4res = BodyFat2$BODYFAT - predict(lmmodel4)
+E=EEMS(100,10)
+E
